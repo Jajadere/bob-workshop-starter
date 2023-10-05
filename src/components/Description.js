3e0ef93e5c3ffb68fe547aea0a452304d5528430
Jajadere <147031124+Jajadere@users.noreply.github.com>
2023-10-05 17:52:28 +0200

    Ny tekst

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,7 +5,7 @@ function Description() {
   return (
     <Box py={5} bgcolor="#e8eaf6" borderRadius={3} boxShadow={3} mt={4} px={3}>
       <Typography variant="h6" gutterBottom color="#3f51b5">
-        Om Meg
+        Utsikt
       </Typography>
       <Typography>
       En fin utsikt kan gi oss glede, inspirasjon og ro. Den kan være over et naturlandskap, en storby eller en himmel full av stjerner. En fin utsikt kan få oss til å stoppe opp og beundre verden rundt oss. 
